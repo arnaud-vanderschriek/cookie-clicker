27f80a46ea2710b83c79a98ec5f5c1263233abb3
Shaunyx <e_jeremy@outlook.com>
2019-09-24 16:36:46 +0200

    script wip

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,44 @@
 //variable utilisée
+(() => {
+  var counterClick = 0; //renmagasine les valeurs counters des autres évents
+  var counterCandyCane; //nbr de bonus candyCane
+  var counterMultiply; //nbr de bonus multiply
 
-var counterClick = 0; //renmagasine les valeurs counters des autres évents
-var counterCandyCane; //nbr de bonus candyCane
-var counterMultiply; //nbr de bonus multiply
+  var price;
+  var priceEmilie;
+  var priceCandyCane;
 
-var price;
-var priceEmilie;
-var priceCandyCane;
+  //code Here
 
-//code Here
+  /*functions Liste*/
 
-/*functions Liste*/
-
-//Function Click
-
-function click() {
+  //One Click, one by one and update the counter
   document.getElementById("cookie").addEventListener("click", () => {
     counterClick = counterClick + 1;
-    console.log("click");
-    console.log("counterClick");
+    //console.log(counterClick);
   });
-}
 
-//Function Candycane/autoclick
+  //Function Candycane/autoclick
 
-//Function Multiplier
+  //Function Multiplier
 
-//Function Disable
-function disable() {
-  if (counterClick < price) {
-    document.getElementById("emilie").disable = true;
-    document.getElementById("candycane").disable = true;
-  } else {
-    document.getElementById("emilie").disable = false;
-    document.getElementById("candycane").disable = false;
+  //Function Disable
+  function disable() {
+    if (counterClick < price) {
+      document.getElementById("emilie").disable = true;
+      document.getElementById("candycane").disable = true;
+    } else {
+      document.getElementById("emilie").disable = false;
+      document.getElementById("candycane").disable = false;
+    }
   }
-}
-setInterval(disable, 500);
+  setInterval(disable, 500);
 
-//Function PetitDej
+  //Function PetitDej
 
-//mettre a jour le counter ( avec setInterval ? )
-function updateCounter() {
-  document.getElementById("counter").innerText = counterClick;
-}
-setInterval(updateCounter, 500);
+  //mettre a jour le counter ( avec setInterval ? )
+  function updateCounter() {
+    document.getElementById("counter").innerText = counterClick;
+  }
+  setInterval(updateCounter, 500);
+})();
